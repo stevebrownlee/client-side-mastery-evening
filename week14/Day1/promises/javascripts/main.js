45abe9caf190466cddf91401aab33ea14a1eb248
Zoe Ames <zoe.ames@ingramcontent.com>
2016-10-10 13:26:09 -0500

    promises lecture added student starter code

--- week14/Day1/promises/javascripts/main.js
+++ week14/Day1/promises/javascripts/main.js
@@ -40,18 +40,20 @@ $(document).ready(function() {
   }
 
 
-  function doSomeStuffWhenAjaxIsDone(){
-    /*
-      Loop through all the songs and build the
-      DOM elements for each one
-     */
+  getSongs().then(function(dataPass){
+    return getSongs2(dataPass);
+  }).then(function(dataPass2){
+    console.log("dataPass inside 2nd then", songs);
+    console.log("dataPass2", dataPass2);
+    dataPass2.songs.forEach(function(song){
+      songs.push(song);
+    });
+    
+    console.log("fin", songs);
+    
     for (var i = 0; i < songs.length; i++) {
       currentSong = songs[i];
 
-      /*
-        Build up some HTML to display the songs,
-        artists, and albums
-       */
       songData += "<div class='song-block'>";
         songData += "<h1>" + currentSong.title + "</h1>";
         songData += "<div class='artist'>Performed by ";
@@ -65,17 +67,66 @@ $(document).ready(function() {
 
     console.log("songData", songData);
     contentEl.html(songData);
-  }
-
-  getSongs().then(function(dataPass){
-    return getSongs2(dataPass);
-  }).then(function(dataPass2){
-    console.log("dataPass inside 2nd then", songs);
-    console.log("dataPass2", dataPass2);
-    dataPass2.songs.forEach(function(song){
-      songs.push(song);
-    })
-    console.log("fin", songs);
-    doSomeStuffWhenAjaxIsDone();
   });
 });
+
+
+
+
+
+
+
+
+
+
+// **********************************************************
+//                STARTER CODE GIVEN TO STUDENTS
+// **********************************************************
+
+// "use strict";
+
+// $(document).ready(function() {
+//   console.log("jquery is ready");
+//   var contentEl = $("#all-my-songs");
+//   var songs = [];
+//   $.ajax({
+//     url: "../songs.json"
+//   }).done(function(data) {
+//     songs = data.songs;
+//     console.log("data", data);
+//     $.ajax({
+//       url: "../songs2.json"
+//       }).done(function(data2) {
+//         console.log("data2", data2);
+//         var songData = "";
+//         var currentSong;
+        
+//         data2.songs.forEach(function(song){
+//           songs.push(song);
+//         });
+
+//         console.log("songs", songs);
+        
+//         for (var i = 0; i < songs.length; i++) {
+//           currentSong = songs[i];
+
+//           songData += "<div class='song-block'>";
+//             songData += "<h1>" + currentSong.title + "</h1>";
+//             songData += "<div class='artist'>Performed by ";
+//               songData += currentSong.artist;
+//             songData += "</div>";
+//             songData += "<div class='album'>On the album ";
+//               songData += currentSong.album;
+//             songData += "</div>";
+//           songData += "</div>";
+//         }
+
+//         console.log("songData", songData);
+//         contentEl.html(songData);
+//       }).fail(function(error) {
+//         console.log( "error" , error);
+//       });
+//   }).fail(function(error) {
+//     console.log( "error" , error);
+//   });
+// });
