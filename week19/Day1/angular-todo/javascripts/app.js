34b00a588af04e4c9d5676da9a4164adad223774
Zoe Ames <zoeames@gmail.com>
2016-11-14 09:01:10 -0600

    initial angular stuff, partials

--- week19/Day1/angular-todo/javascripts/app.js
+++ week19/Day1/angular-todo/javascripts/app.js
@@ -1,5 +1,72 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name:"New Item"}];
+});
+
+app.controller("TodoCtrl", function($scope){
+  $scope.welcome = "hello";
+      $scope.showListView = true;
+      $scope.newTask = {};
+      $scope.items = [
+        {
+          id: 0,
+          task: "mow the lawn",
+          isCompleted: true,
+          dueDate: "12/5/17",
+          assignedTo: "greg",
+          location: "Zoe's house",
+          urgency: "low",
+          dependencies: "sunshine, clippers, hat, water, headphones"
+        },
+        {
+          id: 1,
+          task: "grade quizzes",
+          isCompleted: false,
+          dueDate: "12/5/15",
+          assignedTo: "Joe",
+          location: "NSS",
+          urgency: "high",
+          dependencies: "wifi, tissues, vodka"
+        },
+        {
+          id: 2,
+          task: "take a nap",
+          isCompleted: false,
+          dueDate: "5/21/16",
+          assignedTo: "zoe",
+          location: "Zoe's house",
+          urgency: "medium",
+          dependencies: "hammock, cat, pillow, blanket"
+        }
+      ];
+
+      $scope.newItem = function(){
+          console.log("you clicked new Item");
+          $scope.showListView = false;
+      };
+      
+      $scope.allItem = function(){
+        console.log("you clicked all items");
+        $scope.showListView = true;
+      };
+
+      $scope.addNewItem = function(){
+        $scope.newTask.isCompleted = false;
+        $scope.newTask.id = $scope.items.length;
+        console.log("you added a new item", $scope.newTask);
+        $scope.items.push($scope.newTask);
+        $scope.newTask="";
+      };
 
 
+
+
+
+
+
+
+
+});
