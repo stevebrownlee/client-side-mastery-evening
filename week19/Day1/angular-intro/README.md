50a3180ce3d31a447e3b9b476676e8254e6d349e
Zoe Ames <zoeames@gmail.com>
2016-11-14 08:07:55 -0600

    added angular intro

---
+++ week19/Day1/angular-intro/README.md
@@ -0,0 +1,5 @@
+Just clone this puppy and run `http-server` (If you have it. Otherwise, `npm install http-server -g` (If you have node installed. Otherwise, just forget it, man.)).
+
+Uncomment each section in the HTML to see each bit in action.
+
+Most examples are pulled from http://www.angularjsbook.com by Chris Smith. Check it out for a very clear and concise introduction to Angular.
---
+++ week19/Day1/angular-intro/app.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('app', []);
+
+myApp.config(['$controllerProvider', function($controllerProvider) {
+  $controllerProvider.allowGlobals();
+}]);
+
+// The other way to write a controller to make it part of the module:
+// myApp.controller('MessageController', ['$scope', function ($scope) {
+function MessageController($scope) {
+  $scope.message  = "This is a model.";
+}
+
+function CountController($scope) {
+  $scope.count = function() { return 12; }
+}
+
+// Two controllers with the same 'name' property
+function NameController($scope) {
+  $scope.name = "First";
+}
+
+function SecondNameController($scope) {
+  $scope.name = "Second";
+}
+// *************************
+
+function SumController($scope) {
+  $scope.values = [1,2];
+  $scope.newValue = 1;
+  $scope.add = function() {
+    $scope.values.push(parseInt($scope.newValue));
+  };
+  $scope.$watch('values', function () {
+    $scope.sum = $scope.values.reduce(function(a, b) {
+      return a + b;
+    });
+  }, true);
+}
+
+function EscapeController($scope, $element) {
+  $scope.message = '';
+  $element.bind('keyup', function (event) {
+    if (event.keyCode === 27) { // esc key
+      console.log($element);
+      $scope.$apply(function() {
+        $scope.message = '';
+      });
+    }
+  });
+}
+
+// Creating and using a service
+myApp.value('score', {points: 0});
+
+myApp.controller('ScoreController', function($scope, score) {
+  $scope.score = score;
+  $scope.increment = function() {
+    $scope.score.points++;
+  };
+});
