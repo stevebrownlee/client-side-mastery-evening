c88bea0b96baf371bafb226df6b45232ea73181d
Zoe Ames <zoeames@gmail.com>
2016-11-19 08:32:25 -0600

    partials working

--- week19/Labday/angular-todo/javascripts/controllers/ItemListCtrl.js
+++ week19/Labday/angular-todo/javascripts/controllers/ItemListCtrl.js
@@ -1,3 +1,24 @@
-app.controller("ItemListCtrl", function($scope){
-  $scope.message = "ItemListCtrl";
+app.controller("ItemListCtrl", function($scope, ItemFactory){
+  	$scope.message = "ItemListCtrl";
+	$scope.newTask = {};
+	$scope.items = [];
+
+	let getItems = function(){
+		ItemFactory.getItemList().then(function(fbItems){
+			$scope.items = fbItems;
+		}, function(error){
+			console.log("error", error);
+			$scope.items = [];
+		});  
+	}
+
+	getItems();
+
+
+
+	$scope.deleteItem = function(itemId){
+		ItemFactory.deleteItem(itemId).then(function (response) {
+			getItems();              
+		});
+	}
 });
--- week19/Labday/angular-todo/javascripts/controllers/ItemNewCtrl.js
+++ week19/Labday/angular-todo/javascripts/controllers/ItemNewCtrl.js
@@ -1,3 +1,11 @@
-app.controller("ItemNewCtrl", function($scope){
+app.controller("ItemNewCtrl", function($scope, ItemFactory, $location){
   $scope.message = "ItemNewCtrl";
+
+  	$scope.addNewItem = function(){
+        $scope.newTask.isCompleted = false;
+        ItemFactory.postNewItem($scope.newTask).then(function (response) {
+          $scope.newTask = {};
+          $location.url("/items/list");
+        });
+    };
 });
