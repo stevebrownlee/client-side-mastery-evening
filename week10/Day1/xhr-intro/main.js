675360f832082874f4e739458d7ca19cf6eab271
Zoe Ames <zoeames@gmail.com>
2017-03-06 13:32:03 -0600

    XHR intro lesson done

--- week10/Day1/xhr-intro/main.js
+++ week10/Day1/xhr-intro/main.js
@@ -6,43 +6,40 @@ function executeThisCodeIfXHRFails () {
 }
 
 function executeThisCodeAfterFileLoaded () {
-  console.log("executeThisCodeAfterFileLoaded time:", Date.now());
+  console.log("executeThisCodeAfterFileLoaded time:", Date.now()); //should be about 40ms after last one.
+  console.log("this:", this); //return XHR request
+  console.log("this.responseText:", this.responseText);//return json data
+  var data = JSON.parse(this.responseText); //must parse because it comes back as a string and we want to use it like an object
+  console.log("data", data);//object ready to go
 
-
-  console.log("this.responseText:", this.responseText);
-  var data = JSON.parse(this.responseText);
-  console.log("data", data);
-
-  var contentEl = document.getElementById("all-my-songs")
+  var contentEl = document.getElementById("all-my-cats")
   console.log("contentEl", contentEl);
 
-  var songData = "";
-  var currentSong;
+  var catData="";
+  var currentCat;
 
-  for (var i = 0; i < data.songs.length; i++) {
-    currentSong = data.songs[i];
+  for (var i = 0; i < data.cats.length; i++) {
+    currentCat = data.cats[i];
 
-    songData += "<div class='song-block'>";
-      songData += `<h1>${currentSong.title}</h1>`;
-      songData += "<div class='artist'>Performed by ";
-        songData += currentSong.artist;
-      songData += "</div>";
-      songData += "<div class='album'>On the album ";
-        songData += currentSong.album;
-      songData += "</div>";
-    songData += "</div>";
+    catData += `<div class="col-sm-6 col-md-4"><div class="thumbnail">`
+    catData += `<img src="${currentCat.url}" alt="...">`
+    catData += `<div class="caption">`
+    catData += `<h3 class="text-center">${currentCat.name}</h3>`
+    catData += `<p>Color: ${currentCat.color}</p>`
+    catData += `<p>Age: ${currentCat.age}</p>`
+    catData += `</div></div></div>`
   };
 
-  console.log(songData);
-  contentEl.innerHTML = songData;
+  console.log(catData);
+  contentEl.innerHTML = catData;
 }
 
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
-myRequest.addEventListener("error", executeThisCodeIfXHRFails)
-myRequest.open("GET", "songs.json")
-myRequest.send();
+var myRequest = new XMLHttpRequest();  //new constructor - has all methods available
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded); //when the file loads call this function
+myRequest.addEventListener("error", executeThisCodeIfXHRFails); //if the file fails to load call this function
+myRequest.open("GET", "cats.json"); //initializes the request;  what action are you trying to perform (get) on what file (songs.json)
+myRequest.send();//sends the request - aka gets the json;
 
 
 console.log("Last line in JS file");
-console.log(Date.now());
+console.log(Date.now()); //should be about 1 ms after first one
