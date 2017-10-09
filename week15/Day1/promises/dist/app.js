d13a66fffdc1cdf8344dc71279995170fc6c3b3b
Zoe Ames <zoe.ames@ingramcontent.com>
2017-10-09 10:30:19 -0500

    added catch

--- week15/Day1/promises/dist/app.js
+++ week15/Day1/promises/dist/app.js
@@ -66,25 +66,54 @@ var dinosaurs = [];
 
 
   // one possible way to resolve the promises - not great because still looks like pyramid
+ //  var dinoGetter = function(){
+	//   firstDinosaurJSON().then(function(results){
+	//     // console.log("results", results);
+	//     results.forEach(function(dino){
+	//       dinosaurs.push(dino);
+	//     });	    
+	//     secondDinosaurJSON().then(function(results2){
+	//       // console.log("results2", results2);
+	//       results2.forEach(function(dino){
+	//       	dinosaurs.push(dino);
+	//     	});
+	//       thirdDinosaurJSON().then(function(results3){
+	//         // console.log("results3", results3);
+	//         results3.forEach(function(dino){
+	//       		dinosaurs.push(dino);
+	//     		});
+	//     		makeDinos();
+	//       }).catch(function(err){
+	//       	console.log("error", err);
+	//       });
+	//     });
+	//   });
+	// };
+
+
+  //much better solution - chained promises.  however we don't have access to results or results2 in the final .then.
   var dinoGetter = function(){
 	  firstDinosaurJSON().then(function(results){
 	    // console.log("results", results);
 	    results.forEach(function(dino){
-	      dinosaurs.push(dino);
-	    });	    
-	    secondDinosaurJSON().then(function(results2){
-	      // console.log("results2", results2);
-	      results2.forEach(function(dino){
-	      	dinosaurs.push(dino);
-	    	});
-	      thirdDinosaurJSON().then(function(results3){
-	        // console.log("results3", results3);
-	        results3.forEach(function(dino){
-	      		dinosaurs.push(dino);
-	    		});
-	    		makeDinos();
-	      });
-	    });
+		    dinosaurs.push(dino);
+		  });	
+	    return secondDinosaurJSON();
+	  }).then(function(results2){
+	    // console.log("results2", results2);
+	    results2.forEach(function(dino){
+		    dinosaurs.push(dino);
+		  });	
+	    return thirdDinosaurJSON();
+	  }).then(function(results3){
+	    // console.log("results2 in promise3", results2);//undefined
+	    // console.log("results3", results3);
+	    results3.forEach(function(dino){
+		    dinosaurs.push(dino);
+		  });	
+	 		makeDinos();
+	  }).catch(function(err){
+    	console.log("error", err);
 	  });
 	};
 
