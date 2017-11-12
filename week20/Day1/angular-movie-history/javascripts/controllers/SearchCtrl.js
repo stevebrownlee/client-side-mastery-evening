f840167f7e4a55cd602af51531649ac7e1dee280
Zoe Ames <zoeames@gmail.com>
2017-11-12 16:30:23 -0600

    search

--- week20/Day1/angular-movie-history/javascripts/controllers/SearchCtrl.js
+++ week20/Day1/angular-movie-history/javascripts/controllers/SearchCtrl.js
@@ -1,5 +1,17 @@
 'use strict';
 
-app.controller("SearchCtrl", function($scope) {
-	
+app.controller("SearchCtrl", function($scope, tmdbService) {
+  $scope.movies = [];
+
+  $scope.enterPush = (event) => {
+    if(event.keyCode === 13){
+      tmdbService.searchMovies(event.target.value).then((result) =>{
+        $scope.movies = result;
+      }).catch((err) =>{
+        console.log("searchMovies error", err);
+      });
+    }
+  };
+  
+
 });
