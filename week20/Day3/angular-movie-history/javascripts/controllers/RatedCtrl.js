e0a69b4126be5ef6ec5103376077847467ccd877
Zoe Ames <zoeames@gmail.com>
2017-11-14 08:37:46 -0600

    get rated and get wishlist done

--- week20/Day3/angular-movie-history/javascripts/controllers/RatedCtrl.js
+++ week20/Day3/angular-movie-history/javascripts/controllers/RatedCtrl.js
@@ -1,5 +1,11 @@
 'use strict';
 
-app.controller("RatedCtrl", function($scope) {
-	
+app.controller("RatedCtrl", function($scope,  $rootScope, MovieService) {
+  $scope.movies = [];
+
+  MovieService.getRatedMovies($rootScope.uid).then((results) =>{
+    $scope.movies =results;
+  }).catch((err) => {
+    console.log("errror in getRatedMovies", err);
+  });
 });
--- week20/Day3/angular-movie-history/javascripts/controllers/WishlistCtrl.js
+++ week20/Day3/angular-movie-history/javascripts/controllers/WishlistCtrl.js
@@ -1,5 +1,11 @@
 'use strict';
 
-app.controller("WishlistCtrl", function($scope) {
-	
+app.controller("WishlistCtrl", function ($rootScope, $scope, MovieService) {
+  $scope.movies = [];
+
+  MovieService.getWishlistMovies($rootScope.uid).then((results) => {
+    $scope.movies = results;
+  }).catch((err) => {
+    console.log("errror in getRatedMovies", err);
+  });
 });
