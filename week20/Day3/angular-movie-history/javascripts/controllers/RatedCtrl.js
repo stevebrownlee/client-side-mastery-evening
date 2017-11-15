17b06b0fb49fb93f7542a4a1db1f7af68ac74fd9
Zoe Ames <zoeames@gmail.com>
2017-11-15 14:52:22 -0600

    can delete movies

--- week20/Day3/angular-movie-history/javascripts/controllers/RatedCtrl.js
+++ week20/Day3/angular-movie-history/javascripts/controllers/RatedCtrl.js
@@ -3,9 +3,21 @@
 app.controller("RatedCtrl", function($scope,  $rootScope, MovieService) {
   $scope.movies = [];
 
-  MovieService.getRatedMovies($rootScope.uid).then((results) =>{
-    $scope.movies =results;
-  }).catch((err) => {
-    console.log("errror in getRatedMovies", err);
-  });
+  const getMovies = () => {
+    MovieService.getRatedMovies($rootScope.uid).then((results) =>{
+      $scope.movies =results;
+    }).catch((err) => {
+      console.log("errror in getRatedMovies", err);
+    });
+  };
+
+  getMovies();
+
+  $scope.deleteMovie = (movie) => {
+    MovieService.deleteMovie(movie.id).then((results) =>{
+      getMovies();
+    }).catch((err) => {
+      console.log("errror in deleteMovie", err);
+    });
+  };
 });
