a17da1c02a4eb2be895065780caef913867b2200
Zoe Ames <zoeames@gmail.com>
2017-11-15 14:21:39 -0600

    watched is saving

--- week20/Day3/angular-movie-history/javascripts/controllers/SearchCtrl.js
+++ week20/Day3/angular-movie-history/javascripts/controllers/SearchCtrl.js
@@ -1,17 +1,37 @@
 'use strict';
 
-app.controller("SearchCtrl", function($scope, tmdbService) {
+app.controller("SearchCtrl", function ($location, $rootScope, $scope, MovieService, tmdbService) {
   $scope.movies = [];
 
   $scope.enterPush = (event) => {
-    if(event.keyCode === 13){
-      tmdbService.searchMovies(event.target.value).then((result) =>{
+    if (event.keyCode === 13) {
+      tmdbService.searchMovies(event.target.value).then((result) => {
         $scope.movies = result;
-      }).catch((err) =>{
+      }).catch((err) => {
         console.log("searchMovies error", err);
       });
     }
   };
-  
+
+  const createMovie = (movie) => {
+    return {
+      "title": movie.title,
+      "overview": movie.overview,
+      "poster_path": movie.poster_path,
+      "rating": 0,
+      "isWatched": true,
+      "uid": $rootScope.uid
+    };
+  };
+
+  $scope.saveWatched = (selectedMovie) => {
+    let newMovie = createMovie(selectedMovie);
+    MovieService.postNewMovie(newMovie).then((result) => {
+      $location.path('/rated');
+    }).catch((error) => {
+      console.log("error in postNewMovie", error);
+    });
+  };
+
 
 });
