4cd8d04d18e230e30c5611ba1da108c7e5d33c73
lrouse <lauren.rouse@ingramcontent.com>
2018-03-05 13:20:01 -0600

    Added Obj1 and Function4

--- week04/1Monday/main.js
+++ week04/1Monday/main.js
@@ -43,18 +43,18 @@ document.getElementById("challenge-1b").innerHTML += bestWord;
 
 // CHALLENGE #3
 
-var input = [ 1, 1, 1, 2, 1, 1 ];  // 2
+var input3 = [ 1, 1, 1, 2, 1, 1 ];  // 2
 var repeater, unique;
 
-if (input[0] === input[input.length-1]) {
-    repeater = input[0];
-} else if (input[0] === input[1]) {
-    repeater = input[i];
+if (input3[0] === input3[input3.length-1]) {
+    repeater = input3[0];
+} else if (input3[0] === input3[1]) {
+    repeater = input3[i];
 }
 
-for (var i = 0; i < input.length; i++){
-    if (input[i] !== repeater){
-        unique = input[i];
+for (var i = 0; i < input3.length; i++){
+    if (input3[i] !== repeater){
+        unique = input3[i];
     }
 }
 
@@ -63,12 +63,27 @@ document.getElementById("challenge-3").innerHTML += unique;
 // CHALLENGE #3 with Sort Method
 var element3b = document.getElementById("challenge-3b");
 
-input.sort(function(a, b){ return a - b });
-if (input[0]===input[1]){
-    element3b.innerHTML += input.pop();
+input3.sort(function(a, b){ return a - b });
+if (input3[0]===input3[1]){
+    element3b.innerHTML += input3.pop();
 } else {
     element3b.innerHTML += element[0];
 }
+
+// CHALLENGE #4
+var input4 = [ 3, 8, 1, 2, 4, 12 ];  // [ 6, 16, 2, 4, 8, 24 ]
+var answer4 = [];
+for (var i = 0; i < input4.length; i++){
+    answer4[i] = input4[i] * 2;
+}
+document.getElementById("challenge-4").innerHTML += answer4;
+
+// CHALLENGE #4 with map
+var answer4b = [];
+var answer4b = input4.map(function(item){
+    return item * 2;
+})
+document.getElementById("challenge-4b").innerHTML += answer4b;
 
 // CHALLENGE #5
 var array1 = [1, 2, 4, 7, 5, 9];
@@ -93,26 +108,60 @@ var finalArray = array1.filter(function(num){
     return array2.indexOf(num) === -1;
 })
 
-document.getElementById("challenge-5b").innerHTML += array1;
+document.getElementById("challenge-5b").innerHTML += finalArray;
 
-// CHALLENGE #7
+/// OBJECTS ///
 
-var numBeggers = 3;
-var offerings = [1, 2, 3, 4, 5];
-var result = [];
-// first one takes [1, 4]=5 second one takes [2, 5]=7 second one takes [3]=3
-// display [5, 7, 3]
-function beggars(values, n) {
-    var final = [];
-    for (var i = n; 0 < n; i--) {
-        values.forEach((value, index) => {
-            var finalIndex = i - n;
-            if (index % i === 0) {
-                final[finalIndex] += value
-            }
-        })
+// CHALLENGE #1 with reduce
+
+var players = [ { name: "Bob", scores: [10, 65] },
+                { name: "Bill", scores: [90, 5] },
+                { name: "Charlie", scores: [40, 55] }
+              ]; // Displays "Bill"
+var currentTotal = 0;
+var winner = { total: 0 }
+
+players.forEach(function(player){
+    player.total = player.scores.reduce(function(prev, curr){ return prev + curr });
+});
+
+players.forEach(function(player){
+    if (player.total <= 100 && player.total > winner.total){
+        winner = player;
     }
-    return final;
+});
+document.getElementById("challenge-obj-1b").innerHTML += winner.name + " with " + winner.total;
+
+/// FUNCTIONS ///
+
+function writeToDom(input, id){
+    document.getElementById(id).innerHTML += input;
 }
 
-beggars(offerings, numBeggers);
+// CHALLENGE #4 with splice
+
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+function challenge4(birds){
+    var geeseIndexes = findGeese(birds);
+    removeGeese(geeseIndexes, birds);
+    writeToDom(birds.join(", "), "challenge-fxn-4b")
+}
+
+function findGeese(birds){
+    indexes = [];
+    geese.forEach(function(goose){
+        if (birds.indexOf(goose) !== -1){
+            indexes.push(birds.indexOf(goose));
+        }
+    });
+    return indexes;
+}
+
+function removeGeese(geeseIndexes, birds){
+    geeseIndexes.sort(function(a, b,){return b - a}).forEach(function(i){
+        birds.splice(i, 1);
+    });
+}
+
+challenge4(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]) //["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
