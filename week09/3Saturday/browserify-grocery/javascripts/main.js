a55055ed80933006a1807e533172d1b68d3bf42d
Zoe Ames <zoeames@gmail.com>
2018-04-15 19:00:49 -0500

    plus minus works

--- week09/3Saturday/browserify-grocery/javascripts/main.js
+++ week09/3Saturday/browserify-grocery/javascripts/main.js
@@ -1,3 +1,77 @@
 const dataGatekeeper = require('./dataGatekeeper');
 
 dataGatekeeper.initializer();
+
+// FROM: https://www.codeply.com/go/2VmBU7TanF/bootstrap-plus-minus-counter-input
+$('body').on('click', '.btn-number', function (e) {
+  e.preventDefault();
+
+  const fieldName = $(this).attr('data-field');
+  const type = $(this).attr('data-type');
+  const input = $(`input[name="${fieldName}"]`);
+  const currentVal = parseInt(input.val());
+  if (!isNaN(currentVal)) {
+    if (type === 'minus') {
+      if (currentVal > input.attr('min')) {
+        input.val(currentVal - 1).change();
+      } else {
+        $(this).attr('disabled', true);
+      }
+    } else if (type === 'plus') {
+      if (currentVal < input.attr('max')) {
+        input.val(currentVal + 1).change();
+        $(this).parent().parent().find('.minus').attr('disabled', false);
+      } else {
+        $(this).attr('disabled', true);
+      }
+    }
+  } else {
+    input.val(0);
+  }
+});
+$('.input-number').focusin(function () {
+  $(this).data('oldValue', $(this).val());
+});
+$('.input-number').change(function () {
+  const minValue = parseInt($(this).attr('min'));
+  const maxValue = parseInt($(this).attr('max'));
+  const valueCurrent = parseInt($(this).val());
+
+  name = $(this).attr('name');
+  if (valueCurrent >= minValue) {
+    $(`.btn-number[data-type='minus'][data-field='${name}']`).removeAttr(
+      'disabled'
+    );
+  } else {
+    console.error('Sorry, the minimum value was reached');
+    $(this).val($(this).data('oldValue'));
+  }
+  if (valueCurrent <= maxValue) {
+    $(`.btn-number[data-type='plus'][data-field="${name}"]`).removeAttr(
+      'disabled'
+    );
+  } else {
+    console.error('Sorry, the maximum value was reached');
+    $(this).val($(this).data('oldValue'));
+  }
+});
+$('.input-number').keydown(function (e) {
+  // Allow: backspace, delete, tab, escape, enter and .
+  if (
+    $.inArray(e.keyCode, [46, 8, 9, 27, 13, 190,]) !== -1 ||
+    // Allow: Ctrl+A
+    (e.keyCode === 65 && e.ctrlKey === true) ||
+    // Allow: home, end, left, right
+    (e.keyCode >= 35 && e.keyCode <= 39)
+  ) {
+    // let it happen, don't do anything
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if (
+    (e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) &&
+    (e.keyCode < 96 || e.keyCode > 105)
+  ) {
+    e.preventDefault();
+  }
+});
