f4bc899b560343ddbc8b7e276753df54aaf132ca
Zoe Ames <zoeames@gmail.com>
2018-04-18 17:58:09 -0500

    updated week9

--- week09/3Saturday/browserify-grocery/javascripts/departments.js
+++
@@ -1,9 +0,0 @@
-const loadDepartments = (loadFunction, errorFunction) => {
-  const dinoLoader = new XMLHttpRequest();
-  dinoLoader.addEventListener('load', loadFunction);
-  dinoLoader.addEventListener('error', errorFunction);
-  dinoLoader.open('GET', '../db/departments.json');
-  dinoLoader.send();
-};
-
-module.exports = loadDepartments;
--- week09/3Saturday/browserify-grocery/javascripts/items.js
+++
@@ -1,9 +0,0 @@
-const loadItems = (loadFunction, errorFunction) => {
-  const dinoLoader = new XMLHttpRequest();
-  dinoLoader.addEventListener('load', loadFunction);
-  dinoLoader.addEventListener('error', errorFunction);
-  dinoLoader.open('GET', '../db/items.json');
-  dinoLoader.send();
-};
-
-module.exports = loadItems;
