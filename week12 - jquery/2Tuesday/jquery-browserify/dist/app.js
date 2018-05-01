c61e56281df83e659b6693975cde0a99f68bb577
Callan Morrison <callan.l.morrison@gmail.com>
2018-05-01 10:30:20 -0500

    basic stucture for data and printing then binding events

--- week12 - jquery/2Tuesday/jquery-browserify/dist/app.js
+++ week12 - jquery/2Tuesday/jquery-browserify/dist/app.js
@@ -1,6 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const loadFishes = require('./fishes');
 const fishDom = require('./fishDom');
+const bindEvents = require('./events');
 
 const errorFunction = (error) =>  {
   console.error({error: error,});
@@ -10,6 +11,7 @@ const whenFishesLoad = (data) => {
   const fishes = data.fishes;
   const fishStringToPrint = fishDom(fishes);
   $('#available').append(fishStringToPrint);
+  bindEvents();
 };
 
 const initializer = () => {
@@ -18,7 +20,41 @@ const initializer = () => {
 
 module.exports = initializer;
 
-},{"./fishDom":2,"./fishes":3}],2:[function(require,module,exports){
+},{"./events":2,"./fishDom":3,"./fishes":4}],2:[function(require,module,exports){
+// Filter fish that are "on sale"
+const filterFish = () => {
+  $('#available .fish').not('.on-sale').toggle();
+};
+
+const changeButtonText = () => {
+  $('#show-sale').text((i, text) => {
+    let returnText = '';
+    if (text === 'Show Sale Fish') {
+      returnText = 'Show All';
+    } else {
+      returnText = 'Show Sale Fish';
+    }
+    return returnText;
+  });
+};
+
+// Add fish to "Basket"
+const moveToCart = (e) => {
+  const fishCard = $(e.target).closest('.fish');
+  $('#snagged').append(fishCard);
+};
+
+const bindEvents = () => {
+  $('#show-sale').click(() => {
+    changeButtonText();
+    filterFish();
+  });
+  $('button.add').click(moveToCart);
+};
+
+module.exports = bindEvents;
+
+},{}],3:[function(require,module,exports){
 const writeFishes = (fishes) => {
   let domString = '';
   fishes.forEach((fish) => {
@@ -42,7 +78,7 @@ const writeFishes = (fishes) => {
 
 module.exports = writeFishes;
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 const loadItems = (loadFunction, errorFunction) => {
   $.get('/db/fishes.json')
     .done(loadFunction)
@@ -51,9 +87,9 @@ const loadItems = (loadFunction, errorFunction) => {
 
 module.exports = loadItems;
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 const initializer = require('./data');
 
 initializer();
 
-},{"./data":1}]},{},[4]);
+},{"./data":1}]},{},[5]);
--- week12 - jquery/2Tuesday/jquery-browserify/javascripts/data.js
+++ week12 - jquery/2Tuesday/jquery-browserify/javascripts/data.js
@@ -1,5 +1,6 @@
 const loadFishes = require('./fishes');
 const fishDom = require('./fishDom');
+const bindEvents = require('./events');
 
 const errorFunction = (error) =>  {
   console.error({error: error,});
@@ -9,6 +10,7 @@ const whenFishesLoad = (data) => {
   const fishes = data.fishes;
   const fishStringToPrint = fishDom(fishes);
   $('#available').append(fishStringToPrint);
+  bindEvents();
 };
 
 const initializer = () => {
