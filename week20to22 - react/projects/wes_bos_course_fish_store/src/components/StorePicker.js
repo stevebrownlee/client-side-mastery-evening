7804e0c71a520ad35a26d4f10398b8eb7a2c0818
Zoe Ames <zoeames@gmail.com>
2018-05-18 21:54:38 -0500

    added form

--- week20to22 - react/projects/wes_bos_course_fish_store/src/components/StorePicker.js
+++ week20to22 - react/projects/wes_bos_course_fish_store/src/components/StorePicker.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  // You can only ever return one element from a render
+  //  If you need to return siblings use React.Fragment
   render () {
-    return <p>I am the store picker</p>
+    return (
+      <form className="store-selector">
+        { /* This is a comment in JSX */ }
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
