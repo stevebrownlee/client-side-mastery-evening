907915432682196c545237f8325f413e4271499e
Zoe Ames <zoeames@gmail.com>
2018-05-18 21:43:57 -0500

    added first component

--- week20to22 - react/projects/wes_bos_course_fish_store/src/index.js
+++ week20to22 - react/projects/wes_bos_course_fish_store/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hi');
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker></StorePicker>, document.querySelector('#main'));
