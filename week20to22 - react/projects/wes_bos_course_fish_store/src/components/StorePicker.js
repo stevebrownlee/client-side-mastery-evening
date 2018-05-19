d9b74db11d183a3cf3932bd3b4eb6086cb31a57a
Zoe Ames <zoeames@gmail.com>
2018-05-19 06:27:39 -0500

    change route

--- week20to22 - react/projects/wes_bos_course_fish_store/src/components/StorePicker.js
+++ week20to22 - react/projects/wes_bos_course_fish_store/src/components/StorePicker.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = (event) => {
+    // 1. Stop form from submitting
+    event.preventDefault();
+    // 2. get text from input
+    console.log(this.myInput.value.value);
+    const storeName = this.myInput.value.value;
+    // 3. change page to /store/whatever-entered
+    this.props.history.push(`/store/${storeName}`);
+
+  }
   // You can only ever return one element from a render
   //  If you need to return siblings use React.Fragment
   render () {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* This is a comment in JSX */ }
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input
+          type="text"
+          ref={this.myInput}
+          required placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     )
