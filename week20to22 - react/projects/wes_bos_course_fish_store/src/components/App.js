b25390f9b41f917f5187f8e87855bde0992dcb89
Zoe Ames <zoeames@gmail.com>
2018-06-01 22:55:08 -0500

    fishes save to localstorage

--- week20to22 - react/projects/wes_bos_course_fish_store/src/components/App.js
+++ week20to22 - react/projects/wes_bos_course_fish_store/src/components/App.js
@@ -14,10 +14,20 @@ class App extends React.Component {
 
   componentDidMount() {
     const {params} = this.props.match;
+    // first reinstate localstorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if(localStorageRef){
+      this.setState({order: JSON.parse(localStorageRef)});
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
+  }
+
+  componentDidUpdate() {
+    const {params} = this.props.match;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
   }
 
   componentWillUnmount() {
