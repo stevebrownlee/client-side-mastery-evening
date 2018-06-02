d995d19a475c8a2637aa163eafefc0115bb6f7ee
Zoe Ames <zoeames@gmail.com>
2018-06-02 07:54:07 -0500

    can delete

--- week20to22 - react/projects/wes_bos_course_fish_store/src/components/App.js
+++ week20to22 - react/projects/wes_bos_course_fish_store/src/components/App.js
@@ -56,6 +56,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    // 1. take copy of state
+    const fishes = {...this.state.fishes};
+    // 2. update teh state
+    fishes[key] = null;
+    // 3. update state
+    this.setState({fishes});
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -68,6 +77,16 @@ class App extends React.Component {
     // 3. Call setState to update our state object
     this.setState({ order });
   };
+
+  removeFromOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. remove that item from order
+    delete order[key];
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
 
   render() {
     return (
@@ -85,10 +104,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
