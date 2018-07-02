613ace1d615f72c567acc1b3a1e6b6ab508d4379
Zoe Ames <zoeames@gmail.com>
2018-07-01 23:09:08 -0500

    login/register dynamic

--- week20to22 - react/projects/fish-store/src/components/Auth/Auth.js
+++ week20to22 - react/projects/fish-store/src/components/Auth/Auth.js
@@ -4,21 +4,41 @@ import { Link } from 'react-router-dom';
 import './Auth.css';
 
 class Auth extends React.Component {
-  render () {
-    let authButton = '';
-    let otherAction = '';
-    if (this.props.location.pathname === '/login') {
-      authButton = 'Login';
-      otherAction = <Link to='/register'>Need to Register?</Link>;
-    } else {
-      authButton = 'Register';
-      otherAction = <Link to='/login'>Need to Login?</Link>;
-    }
+  state = {
+    authButton: null,
+    otherAction: null,
+    submitAction: null,
+  }
 
+  loginClickEvent = () => {
+    console.error('you clicked login button');
+  };
+
+  registerClickEvent = () => {
+    console.error('you clicked register button');
+  };
+
+  componentDidMount () {
+    if (this.props.location.pathname === '/login') {
+      this.setState({
+        authButton: 'Login',
+        otherAction: <Link to='/register'>Need to Register?</Link>,
+        submitAction: this.loginClickEvent,
+      });
+    } else {
+      this.setState({
+        authButton: 'Register',
+        otherAction: <Link to='/login'>Need to Login?</Link>,
+        submitAction: this.registerClickEvent,
+      });
+    }
+  }
+
+  render () {
     return (
       <div className="Auth">
         <div id="login-form">
-          <h1 className="text-center">{authButton}</h1>
+          <h1 className="text-center">{this.state.authButton}</h1>
           <form className="form-horizontal col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
             <div className="form-group">
               <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
@@ -34,12 +54,12 @@ class Auth extends React.Component {
             </div>
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-10 text-center">
-                {otherAction}
+                {this.state.otherAction}
               </div>
             </div>
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-10">
-                <button type="submit" className="btn btn-default col-xs-12" id="signin-btn">{authButton}</button>
+                <button type="submit" className="btn btn-default col-xs-12" onClick={this.state.submitAction}>{this.state.authButton}</button>
               </div>
             </div>
           </form>
