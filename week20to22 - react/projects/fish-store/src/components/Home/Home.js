158325675b4e3aab4ddc56f6dcda2104fb79e3bc
Zoe Ames <zoeames@gmail.com>
2018-07-05 22:48:53 -0500

    added home icon

--- week20to22 - react/projects/fish-store/src/components/Home/Home.js
+++ week20to22 - react/projects/fish-store/src/components/Home/Home.js
@@ -3,8 +3,22 @@ import React from 'react';
 class Home extends React.Component {
   render () {
     return (
-      <div>
-        HOME PAGE
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <header className="top">
+            <h1>
+              Catch
+              <span className="ofThe">
+                <span className="of">Of</span>
+                <span className="the">The</span>
+              </span>
+              Day
+            </h1>
+            <h3 className="tagline">
+              <span>Fresh Seafood Market</span>
+            </h3>
+          </header>
+        </div>
       </div>
     );
   }
