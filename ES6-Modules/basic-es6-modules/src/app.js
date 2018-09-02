7dd94a54cc45b37bca8b4b980bfcc7970f3dafd3
lrouse <lauren.rouse@ingramcontent.com>
2018-09-01 20:59:05 -0500

    Added ES6 Module folder with basic project with webpack

---
+++ ES6-Modules/basic-es6-modules/src/app.js
@@ -0,0 +1,16 @@
+import {sum, product} from './modules/math-functions';
+import {greeting} from './modules/greeting';
+
+const a = 23;
+const b = 5;
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+
+}
+
+const result = sum(a, b);
+
+document.querySelector('#greeting').innerHTML = greeting;
+document.getElementById('input').innerHTML = `This sum of ${a} and ${b} is ${result}`;
+
---
+++ ES6-Modules/basic-es6-modules/src/modules/greeting.js
@@ -0,0 +1,3 @@
+const greeting = "Hello there!"
+
+export {greeting}
