821414dad608c84c4b97f980e6407d9c34fe5811
Zoe Ames <zoeames@gmail.com>
2018-10-01 14:06:30 -0500

    mathz

--- week07-es6_modules/3Saturday/calculator/javascripts/helpers/maths.js
+++ week07-es6_modules/3Saturday/calculator/javascripts/helpers/maths.js
@@ -1,9 +1,9 @@
 const add = (num1, num2) => {
-  return num1 + num2;
+  return num1*1 + num2*1;
 };
 
 const subtract = (num1, num2) => {
-  return num1 - num2;
+  return num1*1 - num2*1;
 };
 
 const multiply = (num1, num2) => {
