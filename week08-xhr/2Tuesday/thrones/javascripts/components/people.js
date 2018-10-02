e7020a4214b52e8287f06a493f1ec6fffda84245
Zoe Ames <zoe.ames@ingramcontent.com>
2018-10-02 14:21:50 -0500

    starter code from monday

---
+++ week08-xhr/2Tuesday/thrones/javascripts/components/people.js
@@ -0,0 +1,44 @@
+import {printToDom} from './../helpers/util.js';
+import {buildCharacter} from './character.js';
+
+let people = [];
+
+
+const clearPeopleDiv = () => {
+  const peopleDiv = document.getElementById('people');
+  peopleDiv.innerHTML = '';
+};
+
+const personClick = (e) => {
+  const currentId = e.target.closest('.person').id
+  const person = people.find(x => x.id === currentId);
+  clearPeopleDiv();
+  buildCharacter(person);
+};
+
+const createEvents = () => {
+  let personCards = document.getElementsByClassName('person');
+  for(let i=0; i < personCards.length; i++){
+    personCards[i].addEventListener('click', personClick)
+  }
+};
+
+const builder = () => {
+  let domString = '';
+  people.forEach((person) => {
+    domString += `<div class="col-2 person" id="${person.id}">`;
+    domString +=   `<div class="card">`;
+    domString +=     `<img class="card-img-top" src="${person.imageUrl}" alt="Card image cap">`;
+    domString +=     `<div class="card-body">`;
+    domString +=       `<h5 class="card-title">${person.name}</h5>`;
+    domString +=     `</div>`;
+    domString +=   `</div>`;
+    domString += `</div>`;
+  })
+  printToDom(domString, 'people')
+  createEvents();
+};
+
+
+
+export {builder};
---
+++ week08-xhr/2Tuesday/thrones/javascripts/main.js
@@ -0,0 +1,7 @@
+import {builder} from './components/people.js';
+
+const initializeApp = () => {
+  builder();
+};
+
+initializeApp();
