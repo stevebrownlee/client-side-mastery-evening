0ee5d1b0e6264ce0487721dfa2d8309e61565254
rousell <lauren.l.rouse@gmail.com>
2018-10-21 15:41:58 -0500

    Added Boards and Pins Data

--- week11-promises/pinterest/javascripts/components/boards.js
+++ week11-promises/pinterest/javascripts/components/boards.js
@@ -15,16 +15,17 @@ const writeBoards = (boards) => {
   let domString = '';
   console.log(boards);
   boards.forEach((board) => {
+    const boardImg = board.pins[0] ? board.pins[0].image_url : '/db/default-img.jpeg'
     domString += `
-        <div id='${board.id}' class="pcard board-card p-2">
-          <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
+        <div id='${board.id}' class="pcard board-card align-self-start p-2">
+          <img class="card-img-top" src="${boardImg}" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">${board.name}</h5>
             <p class="card-text">${board.pins.length} Pins</p>
           </div>
         </div>
       `
-  })
+  });
   $('#user-boards').html(domString);
 }
 
--- week11-promises/pinterest/javascripts/components/pins.js
+++ week11-promises/pinterest/javascripts/components/pins.js
@@ -16,9 +16,9 @@ const writePins = (pins) => {
   let domString = '';
   pins.forEach((pin) => {
     domString += `
-        <div id='${pin.id}' class="pcard pin-card p-2">
+        <div id='${pin.id}' class="pcard pin-card align-self-start p-2">
           <img class="card-img-top" src="${pin.image_url}" alt="pin image">
-          <a href='${pin.link}' target='_blank'>${shortenLink(pin.link)}</a>
+          <a href='${pin.link}' target='_blank' class='p-2'><button type="button" class="btn btn-light">${shortenLink(pin.link)}</button></a>
         </div>
       `
   })
