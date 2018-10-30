b99f371094fed9d7f51ce6e3c67737dae09d04d9
Zoe Ames <zoe.ames@ingramcontent.com>
2018-10-30 09:33:23 -0500

    movies displaying

--- week12-axios/2Tuesday/shotz/src/javascripts/components/movies.js
+++ week12-axios/2Tuesday/shotz/src/javascripts/components/movies.js
@@ -1,8 +1,33 @@
+import $ from 'jquery';
+
 import movieData from '../data/moviesData';
+
+const writeMovies = (movies) => {
+  let domString = '';
+  movies.forEach((movie) => {
+    domString += `
+      <div id='${movie.id}' class='col-4 cardz'>
+        <div class="card">
+          <div class="card-header">
+            ${movie.name}
+          </div>
+          <div class="card-body">
+          <h5 class="card-title">${movie.genre}</h5>
+          <h5 class="card-title">${movie.releaseDate}</h5>
+          <h5 class="card-title">${movie.description}</h5>
+          <p class="card-text">${movie.locations.length} Locations</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  $('#movie-container').html(domString);
+};
 
 const initializeMoviesView = () => {
   movieData.loadMovies().then((movies) => {
     console.log('movies', movies);
+    writeMovies(movies);
   }).catch((error) => {
     console.error(error);
   });
