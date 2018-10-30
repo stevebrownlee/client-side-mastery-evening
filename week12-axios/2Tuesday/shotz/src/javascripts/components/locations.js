47c35fb70ee1ccf612b26ca85e7bd6c53cb22257
Zoe Ames <zoe.ames@ingramcontent.com>
2018-10-30 10:27:55 -0500

    added shootTime styling

--- week12-axios/2Tuesday/shotz/src/javascripts/components/locations.js
+++ week12-axios/2Tuesday/shotz/src/javascripts/components/locations.js
@@ -1,8 +1,51 @@
+import $ from 'jquery';
+
 import locationData from '../data/locationsData';
+
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-info';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
+const writeLocations = (locations) => {
+  let domString = '';
+  locations.forEach((location) => {
+    domString += `
+      <div id='${location.id}' class='col-3 cardz'>
+        <div class="card">
+          <div class="card-header ${shootTimeClass(location.shootTime)}">
+            ${location.name}
+          </div>
+          <div class="card-body">
+          <img class="card-img-top" src="${location.imageUrl}" alt="${location.name}">
+          <h5 class="card-title">${location.address}</h5>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  $('#location-container').html(domString);
+};
 
 const initializeLocationsView = () => {
   locationData.loadLocations().then((locations) => {
-    console.log('locations', locations);
+    writeLocations(locations);
   }).catch((error) => {
     console.error(error);
   });
