40266ae778c7d8612cf710e024d97ce6df5f0184
Zoe Ames <zoeames@gmail.com>
2018-11-06 02:43:33 -0600

    cleanup done

--- week13 - sass/2Tuesday/quiddich-roster/src/components/Navbar/navbar.js
+++ week13 - sass/2Tuesday/quiddich-roster/src/components/Navbar/navbar.js
@@ -5,14 +5,14 @@ import './navbar.scss';
 const loadNavbar = () => {
   const newString = `
     <nav>
-    <a href="#" class="brand-logo">Quidditch Roster</a>
-    <ul class="nav-links">
-      <li class="active">
-        <a>Home</a>
-      </li>
-    </ul>
-  </nav>
-    `;
+      <a href="#" class="brand-logo">Quidditch Roster</a>
+      <ul class="nav-links">
+        <li class="active">
+          <a>Home</a>
+        </li>
+      </ul>
+    </nav>
+  `;
   $('#nav-container').html(newString);
 };
 
