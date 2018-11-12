46f63ef972f78f24dbf0f6b909d01a109b720011
Zoe Ames <zoeames@gmail.com>
2018-11-12 15:58:38 -0600

    added google button

--- week14 - firebase auth/1Monday/home_for_the_holidays/src/components/Auth/auth.js
+++ week14 - firebase auth/1Monday/home_for_the_holidays/src/components/Auth/auth.js
@@ -1,13 +1,14 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import googleLogin from './glogin.png';
 import checkLoginStatus from '../../helpers/authEvents';
 
 import './auth.scss';
 
 const loginButton = () => {
   const domString = `
-    <button id="googleAuth" class="btn btn-danger">Login</button>
+    <button id="googleAuth" class="btn btn-secondary"><img src='${googleLogin}'></button>
   `;
   $('#auth').html(domString);
   $('#googleAuth').on('click', () => {
