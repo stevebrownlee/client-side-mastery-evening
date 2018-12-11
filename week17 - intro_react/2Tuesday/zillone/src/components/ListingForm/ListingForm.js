d442d7dca345238a4defdc212a0cc77fc9c2b65d
Zoe Ames <zoeames@gmail.com>
2018-12-11 02:05:24 -0600

    login/logout done

---
+++ week17 - intro_react/2Tuesday/zillone/src/components/ListingForm/ListingForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ListingForm.scss';
+
+class ListingForm extends React.Component {
+  render() {
+    return (
+      <div className="col-xs-8 col-xs-offset-2">
+        <h2>Listing form</h2>
+      </div>
+    );
+  }
+}
+
+export default ListingForm;
