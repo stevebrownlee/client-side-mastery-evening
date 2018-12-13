2c09e61681f5c89718a26003f55f256fdde0d7a2
Zoe Ames <zoe.ames@ingramcontent.com>
2018-12-13 15:18:29 -0600

    added props for listings and listingItem

--- week17 - intro_react/3Saturday/zillone/src/components/ListingItem/ListingItem.js
+++ week17 - intro_react/3Saturday/zillone/src/components/ListingItem/ListingItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import listingProps from '../../helpers/propz/listingProp';
 import './ListingItem.scss';
 
+
 class ListingItem extends React.Component {
+  static propTypes = {
+    listing: listingProps.listingShape,
+    index: PropTypes.number,
+    onSelect: PropTypes.func,
+  }
+
   render() {
     const { listing } = this.props;
     return (
--- week17 - intro_react/3Saturday/zillone/src/components/Listings/Listings.js
+++ week17 - intro_react/3Saturday/zillone/src/components/Listings/Listings.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import './Listings.scss';
+import PropTypes from 'prop-types';
 
+import listingProps from '../../helpers/propz/listingProp';
 import ListingItem from '../ListingItem/ListingItem';
 
+import './Listings.scss';
+
 class Listings extends React.Component {
+  static propTypes = {
+    listings: PropTypes.arrayOf(listingProps.listingShape),
+  };
+
   render() {
     const { listings } = this.props;
     const listingsItemComponents = listings.map((listing, index) => (
       <ListingItem listing={listing} index={index} key={listing.id} />
     ));
     return (
-      <div className="col listings">
+      <div className="listings col">
         <h2>Listings</h2>
         <ul>{listingsItemComponents}</ul>
       </div>
