2288bf8835d26d7b07e1609b2d8d2b722c110d0b
Zoe Ames <zoeames@gmail.com>
2019-01-06 21:58:48 -0800

    added other routes

--- week22_23-react_routing/1monday_and_tuesday/react-nutshell/src/App/App.js
+++ week22_23-react_routing/1monday_and_tuesday/react-nutshell/src/App/App.js
@@ -14,6 +14,12 @@ import MyNavbar from '../components/MyNavbar/MyNavbar';
 
 import Auth from '../components/pages/Auth/Auth';
 import Home from '../components/pages/Home/Home';
+import Friends from '../components/pages/Friends/Friends';
+import Weather from '../components/pages/Weather/Weather';
+import Articles from '../components/pages/Articles/Articles';
+import Messages from '../components/pages/Messages/Messages';
+import Events from '../components/pages/Events/Events';
+
 
 import './App.scss';
 import authRequests from '../helpers/data/authRequests';
@@ -90,6 +96,31 @@ class App extends React.Component {
                   path="/auth"
                   authed={this.state.authed}
                   component={Auth}
+                />
+                <PrivateRoute
+                  path="/friends"
+                  authed={this.state.authed}
+                  component={Friends}
+                />
+                <PrivateRoute
+                  path="/articles"
+                  authed={this.state.authed}
+                  component={Articles}
+                />
+                <PrivateRoute
+                  path="/weather"
+                  authed={this.state.authed}
+                  component={Weather}
+                />
+                <PrivateRoute
+                  path="/events"
+                  authed={this.state.authed}
+                  component={Events}
+                />
+                <PrivateRoute
+                  path="/messages"
+                  authed={this.state.authed}
+                  component={Messages}
                 />
               </Switch>
             </div>
--- week22_23-react_routing/1monday_and_tuesday/react-nutshell/src/components/MyNavbar/MyNavbar.js
+++ week22_23-react_routing/1monday_and_tuesday/react-nutshell/src/components/MyNavbar/MyNavbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink as RRNavLink } from 'react-router-dom';
 import {
   Collapse,
   Navbar,
@@ -15,7 +16,7 @@ class MyNavbar extends React.Component {
   static propTypes = {
     isAuthed: PropTypes.bool,
     logoutClickEvent: PropTypes.func,
-  }
+  };
 
   state = {
     isOpen: false,
@@ -23,17 +24,42 @@ class MyNavbar extends React.Component {
 
   render() {
     const { isAuthed, logoutClickEvent } = this.props;
+
+    const buildNavbar = () => {
+      if (isAuthed) {
+        return (
+          <Nav className='ml-auto' navbar>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/friends'>Friends</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/messages'>Messages</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/weather'>Weather</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/events'>Events</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RRNavLink} to='/articles'>Articles</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink onClick={logoutClickEvent}>Logout</NavLink>
+            </NavItem>
+          </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar />;
+    };
+
     return (
       <div className="my-navbar">
-      <Navbar color="dark" dark expand="md">
+        <Navbar color="dark" dark expand="md">
           <NavbarBrand href="/">React Nutshell</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                { isAuthed ? <NavLink onClick={logoutClickEvent}>Logout</NavLink> : ''}
-              </NavItem>
-            </Nav>
+            {buildNavbar()}
           </Collapse>
         </Navbar>
       </div>
