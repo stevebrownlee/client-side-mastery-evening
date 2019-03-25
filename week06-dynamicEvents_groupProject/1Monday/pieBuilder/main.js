18f3e75c4bf9401b56b23e391c161b24417bb1c7
Zoe Ames <zoeames@gmail.com>
2019-03-25 16:47:11 -0500

    form works

--- week06-dynamicEvents_groupProject/1Monday/pieBuilder/main.js
+++ week06-dynamicEvents_groupProject/1Monday/pieBuilder/main.js
@@ -1,1 +1,28 @@
-console.log('hi');
+const ingredientInput = document.getElementById('ingredientInput');
+const addButton = document.getElementById('addButton');
+
+const ingredients = [];
+
+const domStringBuilder = () => {
+
+}
+
+const submitNewItem = (e) => {
+  e.preventDefault();
+  const ingredientValue = ingredientInput.value;
+  const newIngredient = {
+    item: ingredientValue,
+  };
+  ingredients.push(newIngredient);
+  domStringBuilder();
+};
+
+const addEventListeners = () => {
+  addButton.addEventListener('click', submitNewItem)
+};
+
+const init = () => {
+  addEventListeners();
+};
+
+init();
