422f46f7820931121e034a2af7e889ca8f45b688
Zoe Ames <zoeames@gmail.com>
2019-05-19 15:56:54 -0400

    app initialized

--- week14-auth_ERDs/1Monday/birfday/src/javascripts/main.js
+++ week14-auth_ERDs/1Monday/birfday/src/javascripts/main.js
@@ -1,7 +1,20 @@
+import firebase from 'firebase/app';
+// import 'bootstrap';
+
+import apiKeys from './helpers/apiKeys.json';
+
+// import createNavbar from './components/Navbar/navbar';
+// import loginButton from './components/Auth/Auth';
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 
 const init = () => {
-  console.error('test');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  console.error(firebase);
+  // createNavbar();
+  authData.checkLoginStatus();
+  // loginButton();
 };
 
 init();
