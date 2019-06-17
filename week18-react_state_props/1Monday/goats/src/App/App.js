83e18b0007f070b20b579e5647f431d61c85a246
Zoe Ames <zoeames@gmail.com>
2019-06-17 10:10:13 -0500

    goats adding to state

--- week18-react_state_props/1Monday/goats/src/App/App.js
+++ week18-react_state_props/1Monday/goats/src/App/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-danger">TEST</button>
-      </header>
-    </div>
-  );
+const goats = [
+  {name: 'Billy the Kid', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
+  {name: 'Wooly', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
+  {name: 'Vincent van Goat', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
+];
+
+class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    this.setState({ goats });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GOAT YOGA</h1>
+        <button class="btn btn-danger">hi</button>
+      </div>
+    );
+  }
 }
 
 export default App;
