d51c5a8f22fc6256f719de9b18395b5b064a4f13
Zoe Ames <zoeames@gmail.com>
2019-06-17 11:13:02 -0500

    basic goat project

--- week18-react_state_props/1Monday/goats/src/App/App.js
+++ week18-react_state_props/1Monday/goats/src/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import GoatCoral from '../components/GoatCoral/GoatCoral';
 import './App.scss';
 
 const goats = [
-  {name: 'Billy the Kid', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
-  {name: 'Wooly', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
-  {name: 'Vincent van Goat', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
+  {id: 'goat1', name: 'Billy the Kid', age: 2, description:'I like Yoga!', imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg'},
+  {id: 'goat2', name: 'Wooly', age: 85, description:'I like eating grass.', imgUrl: 'https://pbs.twimg.com/profile_images/980547001166295041/eBs20xF4.jpg'},
+  {id: 'goat3', name: 'Vincent van Goat', age: 18, description:'SHHHHHHH', imgUrl: 'https://goatyoga.com/wp-content/uploads/goat-yoga-goat-in-pink-tutu-with-unicorn-horn.jpg'},
+  {id: 'goat4', name: 'Butter', age: 1, description:'RAWRRRR', imgUrl: 'https://i.ytimg.com/vi/cEJy2q27hVk/maxresdefault.jpg'},
+  {id: 'goat5', name: 'Ramsey', age: .5, description:'Nope', imgUrl: 'https://www.psephizo.com/wp-content/uploads/2018/09/1438297793139.jpeg'},
 ];
 
 class App extends React.Component {
@@ -17,10 +21,11 @@ class App extends React.Component {
   }
 
   render() {
+    const { goats } = this.state;
     return (
       <div className="App">
         <h1>GOAT YOGA</h1>
-        <button class="btn btn-danger">hi</button>
+        <GoatCoral goats={goats} />
       </div>
     );
   }
