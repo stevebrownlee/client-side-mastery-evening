99517c6f347b5de7b382cc5287eb7870dbbb0f20
Zoe Ames <zoeames@gmail.com>
2019-06-23 22:00:49 -0500

    setup complete

---
+++ week19-react_CRUD/1Monday/fishstore/src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>hi</h2>
+        <button className="btn btn-danger">HELP</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
