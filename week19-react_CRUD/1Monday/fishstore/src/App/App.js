e7b4a5f09c7847e4f9a8328e5b32478470b9329c
Zoe Ames <zoeames@gmail.com>
2019-06-23 23:47:28 -0500

    auth works

--- week19-react_CRUD/1Monday/fishstore/src/App/App.js
+++ week19-react_CRUD/1Monday/fishstore/src/App/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import firebase from 'firebase';
+
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>hi</h2>
-        <button className="btn btn-danger">HELP</button>
-      </header>
-    </div>
-  );
+import fbConnection from '../helpers/data/connection';
+fbConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authed: true
+        });
+      } else {
+        this.setState({
+          authed: false
+        });
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  }
+
+  render() {
+    const loadComponent = () => {
+      if (this.state.authed) {
+        return <Home />;
+      };
+      return <Auth />;
+    };
+
+    return (
+      <div className="App">
+        {loadComponent()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ week19-react_CRUD/1Monday/fishstore/src/components/Auth/Auth.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import googleImage from './Sign-in-with-Google.png';
+
+import './Auth.scss';
+
+class Auth extends React.Component {
+
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  };
+
+  render() {
+    return (
+      <div className="Auth">
+        <button id="google-auth" className="btn btn-secondary" onClick={this.loginClickEvent}>
+          <img src={googleImage} alt="Google Auth Button" />
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
