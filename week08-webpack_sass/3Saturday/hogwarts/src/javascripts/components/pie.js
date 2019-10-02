18cce71815f1b4649ee9aa09808c4cb36a6b0493
Zoe Ames <zoeames@gmail.com>
2019-10-02 17:41:33 -0500

    hogwarts setup

---
+++ week08-webpack_sass/3Saturday/hogwarts/src/javascripts/components/pie.js
@@ -0,0 +1,7 @@
+import utilities from '../helpers/utilities';
+
+const makePies = () => {
+  utilities.printToDom('shelf', 'I LOVE pie');
+};
+
+export default { makePies };
---
+++ week08-webpack_sass/3Saturday/hogwarts/src/javascripts/main.js
@@ -0,0 +1,10 @@
+import 'bootstrap';
+
+import '../styles/main.scss';
+import pie from './components/pie';
+
+const init = () => {
+  pie.makePies();
+};
+
+init();
