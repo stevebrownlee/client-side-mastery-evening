4bd21c73114e27db8ee2f0d9c0ddde077740a25a
Zoe Ames <zoeames@gmail.com>
2019-11-03 13:05:30 -0600

    starter code

---
+++ week13-firebase/1Monday/vending-machine/src/javascripts/helpers/data/snackPositionsData.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getSnackPositionsByMachineId = (machineId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/snackPositions.json?orderBy="machineId"&equalTo="${machineId}"`).then((response) => {
+    const positions = [];
+    Object.keys(response.data).forEach((fbId) => {
+      response.data[fbId].id = fbId;
+      positions.push(response.data[fbId]);
+    });
+    resolve(positions);
+  }).catch((error) => reject(error));
+});
+
+export default { getSnackPositionsByMachineId };
