083d23142d9bf134de50fd60959b05eaa9cd2480
Zoe Ames <zoeames@gmail.com>
2019-11-30 12:58:44 -0800

    boards data coming back

--- week18-react_state_props/3Saturday/react-pinterest/src/components/BoardContainer/BoardContainer.js
+++ week18-react_state_props/3Saturday/react-pinterest/src/components/BoardContainer/BoardContainer.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import authData from '../../helpers/data/authData';
+import boardData from '../../helpers/data/boardData';
+
 import './BoardContainer.scss';
 
 class BoardContainer extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => {
+        this.setState({ boards });
+      })
+      .catch(err => console.error('error with get boards request', err));
+  }
 
   render() {
     return (
