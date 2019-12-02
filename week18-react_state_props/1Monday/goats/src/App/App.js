5ce3b6af4307702e3c6849606b0b929e303172e3
Zoe Ames <zoeames@gmail.com>
2019-12-02 16:27:58 -0600

    goats setup complete

---
+++ week18-react_state_props/1Monday/goats/src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-danger">goat</button>
+    </div>
+  );
+}
+
+export default App;
