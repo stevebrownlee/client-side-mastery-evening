fbafd1728c0562ad92539f43f5d5fac0f52a8086
Zoe Ames <zoeames@gmail.com>
2019-12-09 01:24:18 +0530

    pinterest auth

---
+++ week18-react_state_props/2Tuesday/react-pinterest/src/components/BoardContainer/BoardContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './BoardContainer.scss';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <div className="BoardContainer">
+        <h2>Boards</h2>
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
