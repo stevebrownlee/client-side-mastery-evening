29bfa0d792f3ecac68c86e50ba9752996a2718c9
Zoe Ames <zoeames@gmail.com>
2020-02-24 19:56:59 -0600

    WIP

---
+++ week08-modules_jquery/1Monday/README.md
@@ -0,0 +1,17 @@
+# Week 8 - Day 1
+
+> **Agenda:** Simple ES6 Modules
+
+## 50 Shades Demo
+This project is designed to show the problem with global variables.  It has a variable called bookInfo that contains information about the book.
+
+To change the book price:
+* Load the page
+* Open chrome console
+* Type: ```bookInfo.price = 0.01```
+
+## Build out the 50 Shades Project with modules
+* Create `javascripts/helpers/book.js`  with the book info
+* Import that into main.js and show how you can get stuff from other files
+* Create store component
+* Create cart component
---
+++ week08-modules_jquery/1Monday/intro_to_modules/javascripts/components/store.js
@@ -0,0 +1,24 @@
+import cart from './cart.js';
+import bookData from '../helpers/data/bookData.js';
+import util from '../helpers/utilities.js';
+
+const addToCartEvent = (e) => {
+  e.preventDefault();
+  const bookInfo = bookData.getBook();
+  cart.setCart(bookInfo);
+  cart.cartToDom();
+}
+
+const makeStore = () => {
+  const bookInfo = bookData.getBook();
+  let domString = `<h2>Our only book:</h2>`;
+  domString += `Buy now for only`;
+  domString += `<h3 id="book-price">$${bookInfo.price}</h3>`;
+  domString += `<img src=${bookInfo.image} alt="book cover"><br/>`;
+  domString += `<button class="btn btn-danger" id="cartButton">add to cart</button>`;
+
+  util.printToDom('store-container', domString);
+  document.getElementById('cartButton').addEventListener('click', addToCartEvent);
+}
+
+export default { makeStore };
